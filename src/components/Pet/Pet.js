46b19b84ecc195ea378fe783051ac303e05990e8
Yellow <bofengejn@gmail.com>
2023-07-14 20:05:49 +0200

    Update Pet.js

--- src/components/Pet/Pet.js
+++ src/components/Pet/Pet.js
@@ -1,18 +1,23 @@
 const Pet = ({
-    name
+    id,
+    name,
+    description,
+    imageURL,
+    category,
+    likes,
 }) => {
     return (
         <li className="otherPet">
             <h3>Name: {name}</h3>
-            <p>Category: Cat</p>
+            <p>Category: {category}</p>
             <p className="img"><img
-                src="https://pics.clipartpng.com/Cat_PNG_Clip_Art-2580.png" alt="Cat" /></p>
-            <p className="description">This is not my cat Gosho</p>
+                src={imageURL} alt="Cat" /></p>
+            <p className="description">{description}</p>
             <div className="pet-info">
                 <a href="/pet"><button className="button"><i className="fas
                                 fa-heart"></i> Pet</button></a>
                 <a href="/details"><button className="button">Details</button></a>
-                <i className="fas fa-heart"></i> <span> 2</span>
+                <i className="fas fa-heart"></i> <span> {likes}</span>
             </div>
         </li>
     );
